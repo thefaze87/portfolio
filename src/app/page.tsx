3fca25b0dfa4bb127692e90bf40a327ce3c96d66
Mark Fasel <markfasel@gmail.com>
2024-10-06 15:48:24 -0400

    remove text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,12 +190,6 @@ export default function Page() {
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  {/* During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals. */}
                   I’m constantly building, learning, and launching new projects.
                   Recently, I’ve been developing an AI-powered app for marriage
                   using React Native and TailwindCSS to help couples connect

b39c5fe0d07c4c09885831d32b62181e2bf99ac4
Mark Fasel <markfasel@gmail.com>
2024-10-06 17:26:55 -0400

    remove redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  redirects: async () => [
-    {
-      source: '/:path*',
-      has: [{ type: 'header', key: 'host', value: 'markfasel.dev' }],
-      destination: 'https://www.markfasel.dev/:path*',
-      permanent: true
-    }
-  ]
 };
 
 export default nextConfig;
